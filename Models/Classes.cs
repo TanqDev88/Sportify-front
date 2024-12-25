9690a175389550536ac0b3d9936eccf88cf88c17
TanqDev88 <oliverprim783@gmail.com>
2024-12-25 01:59:57 -0300

    ApplicationsUser file is added

--- Models/Classes.cs
+++ Models/Classes.cs
@@ -12,31 +12,41 @@ namespace Sportify_back.Models
 {
     public class Classes
     {
-
+        [Key]
         [Column("IdClasses")]
         public int Id { get; set; }
 
         [Required (ErrorMessage= "El nombre de la clase es obligatorio")]
+        [Display(Name = "Nombre")]
         public string Name { get; set; }
 
-        public Activities? Activities { get; set; }
-
         [ForeignKey("Activities")]
+        [Display(Name = "Actividades")]
         public int  ActivityId { get; set; }
 
+        [ForeignKey("ActivityId")]
+        [Display(Name = "Actividades")]
+        public Activities? Activities { get; set; }
+
         [Required (ErrorMessage= "Debe indicar un día y horario de la clase")]
+        [Display(Name = "Fecha")]
         public DateTime Sched { get; set; }
 
+        [Display(Name = "Profesores")]
         public Teachers? Teachers { get; set; }
 
-        [Display(Name ="Teachers")]
+        [Display(Name ="Profesores")]
+        [ForeignKey("TeachersId")]
         public int  TeachersId { get; set; }
 
         public List<Programmings>? Programmings { get; set; } = new List<Programmings>();
 
         [Required(ErrorMessage= "Debe indicar el cupo máximo de la clase")]
+
+        [Display(Name = "Cupo")]
         public int Quota { get; set; }
 
+        [Display(Name = "Activo")]
         public bool Active { get; set; }
         
     }
